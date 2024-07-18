ccddadcb1afa2a0a0b3484cd0f4e9575274baad2
Cameron <cwitkowski@rogers.com>
2024-07-18 15:27:19 -0700

    Created download data script for SQuAD dataset

---
+++ download_data.py
@@ -0,0 +1,23 @@
+# Goal. Use huggingface rajpurkar/squad dataset and convert it to a .jsonl file
+
+
+import json
+import os
+import transformers
+from tqdm import tqdm
+from datasets import load_dataset
+
+# Load the dataset
+dataset = load_dataset("squad")
+
+# Save the dataset to a .jsonl file
+os.makedirs("data", exist_ok=True)
+with open("data/squad_train.jsonl", "w") as f:
+    for example in tqdm(dataset["train"]):
+        json.dump(example, f)
+        f.write("\n")
+
+with open("data/squad_validation.jsonl", "w") as f:
+    for example in tqdm(dataset["validation"]):
+        json.dump(example, f)
+        f.write("\n")
