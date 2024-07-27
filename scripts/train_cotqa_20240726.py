27277903f6f66650ea149432880eae6d9a4baae2
Alexander Detkov <alexander.d.detkov@gmail.com>
2024-07-27 13:02:03 -0700

    Update train_cotqa_20240726.py
    small fix in cot

--- scripts/train_cotqa_20240726.py
+++ scripts/train_cotqa_20240726.py
@@ -67,7 +67,7 @@ lrs = (3e-4, 1e-4)
 rs = (1, 2, 4)
 batch_size = 16
 base_dir = "data/ballmer_20240726"
-out_dir = "results/ballmer_20240726"
+results_dir = "results/ballmer_20240726"
 num_epochs = 40
 
 
@@ -81,9 +81,11 @@ for train_dataset, validation_dataset in traj_train_traj_validation:
         for r in rs:
             # note: we can ignore the x0_file because there is a 1:1 mapping between x0_file and question_dataset
             question_dataset_basename = os.path.splitext(os.path.basename(train_dataset))[0]
+            
+            folder_name = question_dataset_basename.split("traj_")[1]
+            out_dir = f"{results_dir}/cotqa/{folder_name}_batch{batch_size}_epochs{num_epochs}_lr{lr}_r{r}"
 
-
-            data_gen_func_call = f"python3 train_loop.py --num_epochs {num_epochs} --batch_size {batch_size} --learning_rate {lr} --data_path {train_dataset} --val_path {validation_dataset} --out_dir {out_dir}"
+            data_gen_func_call = f"python3 train_loop.py --num_epochs {num_epochs} --batch_size {batch_size} -r {r} --learning_rate {lr} --data_path {train_dataset} --val_path {validation_dataset} --out_dir {out_dir}"
             
             print(f"[{cnt}] This is the data gen script call: ", data_gen_func_call)
 
